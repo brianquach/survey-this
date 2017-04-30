2bc3aec284de0cb879a4060bd30f4073a641e9fd
Brian Quach <13rianquach@gmail.com>
2017-04-30 14:52:43 -0700

    Fix: remove unncessary splash screen

--- client/mobile/app/navigators/app-navigator.js
+++ client/mobile/app/navigators/app-navigator.js
@@ -1,9 +1,7 @@
 import { StackNavigator } from 'react-navigation';
-import SplashScreen from '../components/scenes/splash-screen';
 import SignIn from '../components/scenes/signin';
 
 
 export const AppNavigator = StackNavigator({
-  Splash: { screen: SplashScreen },
   SignIn: { screen: SignIn }
 });
--- client/mobile/app/reducers/navigation.js
+++ client/mobile/app/reducers/navigation.js
@@ -2,7 +2,7 @@ import { NavigationActions } from 'react-navigation';
 import { AppNavigator } from '../navigators/app-navigator';
 
 
-const initialState = AppNavigator.router.getStateForAction(AppNavigator.router.getActionForPathAndParams('Splash'));
+const initialState = AppNavigator.router.getStateForAction(AppNavigator.router.getActionForPathAndParams('SignIn'));
 
 const nav = (state = initialState, action) => {
   let nextState;
