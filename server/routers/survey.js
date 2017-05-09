1961d17a73c8c99962c71ef7951b1524995dcf23
Brian Quach <13rianquach@gmail.com>
2017-05-08 21:34:18 -0700

    Feat: save survey POST endpoint

--- server/routers/survey.js
+++ server/routers/survey.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const DynamoDB = require('aws-sdk/clients/dynamodb');
+var bodyParser = require('body-parser');
 
 
 const router = express.Router();
@@ -13,18 +14,17 @@ router.use(function timeLog (req, res, next) {
   console.log('Time: ', Date.now())
   next()
 });
+router.use(bodyParser.json());
+//router.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 router.get('/:creator/:filter', function (req, res) {
   const creator = req.params.creator;
 
   var params = {
-    KeyConditionExpression: '#O = :v0',
-    ExpressionAttributeNames: {
-      "#O" : "Owner"
-    },
+    KeyConditionExpression: 'Creator = :v0',
     ExpressionAttributeValues: {
         ":v0": creator,
-    },
+    }
   };
 
   docClient.query(params, function(err, data) {
@@ -32,6 +32,28 @@ router.get('/:creator/:filter', function (req, res) {
       console.log(err, err.stack);
     } else {
       res.send(data);
+    }
+  });
+});
+
+router.post('/', function (req, res) {
+  const creator = req.body.Creator;
+  const title = req.body.Title;
+  const questions = req.body.Questions;
+
+  var params = {
+    Item: {
+      "Creator": creator,
+      "Title": title,
+      "Questions": questions
+    }
+  };
+
+  docClient.put(params, function(err, data) {
+    if (err) {
+      console.log(err, err.stack);
+    } else {
+      res.send({ "IsSuccessful": true });
     }
   });
 });
