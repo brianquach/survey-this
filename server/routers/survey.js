0e98e39e970590589de5c1e5261bddd9e0277c32
Brian Quach <13rianquach@gmail.com>
2017-05-21 19:33:21 -0700

    Feat: store survey results

--- server/routers/survey.js
+++ server/routers/survey.js
@@ -7,40 +7,39 @@ var bodyParser = require('body-parser');
 
 
 const router = express.Router();
-const docClient = new DynamoDB.DocumentClient({
-  params: { TableName: 'Survey' }
-});
+const docClient = new DynamoDB.DocumentClient();
 
-router.use(function timeLog (req, res, next) {
+router.use(function timeLog (request, response, next) {
   console.log('Time: ', Date.now())
   next()
 });
 router.use(bodyParser.json());
 //router.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-router.get('/:creator/:filter', function (req, res) {
-  const creator = req.params.creator;
+router.get('/:creator/:filter', function (request, response) {
+  const creator = request.params.creator;
 
   var params = {
     KeyConditionExpression: 'Creator = :v0',
     ExpressionAttributeValues: {
         ":v0": creator,
-    }
+    },
+    TableName: 'Survey'
   };
 
   docClient.query(params, function(err, data) {
     if (err) {
       console.log(err, err.stack);
     } else {
-      res.send(data);
+      response.send(data);
     }
   });
 });
 
-router.post('/', function (req, res) {
-  const creator = req.body.Creator;
-  const title = req.body.Title;
-  const questions = req.body.Questions;
+router.post('/', function (request, response) {
+  const creator = request.body.Creator;
+  const title = request.body.Title;
+  const questions = request.body.Questions;
   const uuid = uuidV4();
 
   var params = {
@@ -49,14 +48,37 @@ router.post('/', function (req, res) {
       "Title": title,
       "Questions": questions,
       "Id": uuid
+    },
+    TableName: 'Survey'
+  };
+
+  sendResponse();
+  docClient.put(params, function(err, data) {
+    if (err) {
+      console.log(err, err.stack);
+    } else {
+      response.send({ "IsSuccessful": true });
     }
+  });
+});
+
+router.post('/results', function (request, response) {
+  const surveyResults = request.body.surveyResults;
+  const surveyId = request.body.surveyId;
+
+  var params = {
+    Item: {
+      "SurveyId": surveyId,
+      "Results": surveyResults
+    },
+    TableName: 'SurveyResults'
   };
 
   docClient.put(params, function(err, data) {
     if (err) {
       console.log(err, err.stack);
     } else {
-      res.send({ "IsSuccessful": true });
+      response.send({ "IsSuccessful": true });
     }
   });
 });
